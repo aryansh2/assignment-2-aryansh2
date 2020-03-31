aff5f58cc35a23c2bddb4a2c8d2ffa380b9e659c
aryansh2 <54408963+aryansh2@users.noreply.github.com>
2020-03-31 21:55:56 +0530

    MyMain class of problem3 completed with entries

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,11 +1,30 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.main;
-// executable class
-// use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student(8, "Adam"));
+        queue.display();
+        queue.enqueue(new Student(3, "Abhi"));
+        queue.display();
+        queue.enqueue(new Student(1, "Amol"));
+        queue.display();
+        queue.enqueue(new Student(2, "Max"));
+        queue.display();
+        queue.enqueue(new Student(5, "Amit"));
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println("Que Size: " + queue.getSize());
+
+    }
 }
