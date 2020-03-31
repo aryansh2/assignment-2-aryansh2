e6641dd60fc9664ff40c0ae0e1635b8bdd4fafc3
aryansh2 <54408963+aryansh2@users.noreply.github.com>
2020-03-31 21:35:37 +0530

    Initial Commit

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,20 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        for (int i = 0; i < 6; i++) {
+            binarySearchTree.insert((int)(Math.random()*100));
+        }
+        System.out.print("Elements of the binary search tree: ");
+        binarySearchTree.traverse();
+        System.out.print("\nLeft children of the binary search tree: ");
+        binarySearchTree.traverseLeft();
+        System.out.print("\nNo.of Parents not having their left child are: ");
+        System.out.println(binarySearchTree.countLeft(binarySearchTree.getRoot()));
+
+    }
 }
